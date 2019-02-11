9f29b27d9779e526dc6c96fa8e0e028d52cbc184
Karl Riauba <kariauba@kariauba.corpmz.com>
2019-02-10 19:52:17 -0800

    added travis configuration

--- src/App.js
+++ src/App.js
@@ -6,17 +6,19 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+            FUSION, NOT FISSION what humans need 
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
+            FUSION, NOT FISSION what humans need 
+          <img src={logo} height="10%" width="10%" className="App-logo" alt="logo" />
+          <img src={logo} height="20%" width="20%" className="App-logo" alt="logo" />
+          <img src={logo} height="30%" width="30%" className="App-logo" alt="logo" />
           <a
             className="App-link"
             href="https://reactjs.org"
             target="_blank"
             rel="noopener noreferrer"
           >
+            FUSION, NOT FISSION what humans need <code>src/App.js</code> and save to reload.
             Learn React
           </a>
         </header>
